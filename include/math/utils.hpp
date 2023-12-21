2542a4b620ed23a1e2ed5588ddd60b5db2fbed53
pjh9712 <ses88498@gmail.com>
2023-12-22 02:03:27 +0900

    Added skeleton codes for math utils

---
+++ include/math/utils.hpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 Programmers, All Rights Reserved.
+// Authors: Jeonghoon Park
+
+#ifndef MATH__UTILS_HPP_
+#define MATH__UTILS_HPP_
+
+namespace math {}  // namespace math
+
+#endif  // MATH__UTILS_HPP_
---
+++ src/utils.cpp
@@ -0,0 +1,4 @@
+// Copyright (c) 2023 Programmers, All Rights Reserved.
+// Authors: Jeonghoon Park
+
+#include "math/utils.hpp"
