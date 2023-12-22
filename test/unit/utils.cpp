8dc3f882c3415335212b163ba85c461ef7ed465e
pjh9712 <ses88498@gmail.com>
2023-12-22 19:11:26 +0900

    Added utils.cpp for test

---
+++ test/unit/utils.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2023 Programmers, All Rights Reserved.
+// Authors: Jeonghoon Park
+
+#include "gtest/gtest.h"
+
+#include "math/utils.hpp"
+
+namespace math {
+  TEST(MathUtils, Add){}
+}
