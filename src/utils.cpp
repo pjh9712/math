baac4c0dd5a34dc1325ea38d96aa08a776960a70
pjh9712 <ses88498@gmail.com>
2023-12-24 19:02:22 +0900

    Refactor Add Function to Handle infinity Values

--- src/utils.cpp
+++ src/utils.cpp
@@ -11,8 +11,9 @@ auto IsEqual(double lhs, double rhs) -> bool {
   return (std::abs(lhs - rhs) < std::numeric_limits<double>::epsilon());
 }
 auto Add(double lhs, double rhs) -> double {
-  if (std::isnan(lhs) || std::isnan(rhs)) {
-    throw std::invalid_argument("Invalid input: NaN");
+  if (std::isnan(lhs) || std::isnan(rhs) || std::isinf(lhs) ||
+      std::isinf(rhs)) {
+    throw std::invalid_argument("Invalid input: NaN or Infinity");
   }
   return lhs + rhs;
 }
